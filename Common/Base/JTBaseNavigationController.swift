6af8d376693009f41d3f15d19a21296b666f98c5
xuanmeijian <328123464@qq.com>
2017-11-24 16:53:25 +0800

    添加自定义导航栏返回按钮
    添加自定义导航栏返回按钮

--- Common/Base/JTBaseNavigationController.swift
+++ Common/Base/JTBaseNavigationController.swift
@@ -22,11 +22,28 @@ class JTBaseNavigationController: UINavigationController {
         UINavigationBar.appearance().tintColor = UIColor.hexInt(0x333333)
         
         
-        let img = UIImage(named: "btn_back_n")?.resizableImage(withCapInsets: UIEdgeInsetsMake(0, 18, 0, 0))
-        UIBarButtonItem.appearance().setBackButtonTitlePositionAdjustment(UIOffsetMake(-1000, 0), for: .default)    // 让导航条返回键带的title消失!
-        UIBarButtonItem.appearance().setBackButtonBackgroundImage(img, for: .normal, barMetrics: .default)
+//        let img = UIImage(named: "btn_back_n")?.resizableImage(withCapInsets: UIEdgeInsetsMake(0, 18, 0, 0))
+//        UIBarButtonItem.appearance().setBackButtonTitlePositionAdjustment(UIOffsetMake(-1000, 0), for: .default)    // 让导航条返回键带的title消失!
+//        UIBarButtonItem.appearance().setBackButtonBackgroundImage(img, for: .normal, barMetrics: .default)
     }
 
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        
+        if childViewControllers.count > 0 {
+            viewController.hidesBottomBarWhenPushed = true
+           let item = UIBarButtonItem(image: #imageLiteral(resourceName: "btn_back_n").withRenderingMode(.alwaysOriginal), style: .done, target: self, action: #selector(JTBaseNavigationController.pop))
+            
+            viewController.navigationItem.leftBarButtonItem = item
+            self.interactivePopGestureRecognizer?.delegate = viewController as? UIGestureRecognizerDelegate
+        }
+        
+        super.pushViewController(viewController, animated: animated)
+    }
+    
+    @objc func pop() -> Void {
+        self.popViewController(animated: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
